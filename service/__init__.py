d9bb24f2abe6e3231a4328e61f13bc1b95c16196
psd <psd@whatfettle.com>
2014-07-22 13:43:31 +0100

    Added Basic Authentication

--- service/__init__.py
+++ service/__init__.py
@@ -10,6 +10,10 @@ if not app.debug:
     app.logger.addHandler(logging.StreamHandler())
     app.logger.setLevel(logging.INFO)
 
+if app.config.get('BASIC_AUTH_USERNAME'):
+    app.config['BASIC_AUTH_FORCE'] = True
+    basic_auth = BasicAuth(app)
+
 app.logger.info("\nConfiguration\n%s\n" % app.config)
 
 db = SQLAlchemy(app)
