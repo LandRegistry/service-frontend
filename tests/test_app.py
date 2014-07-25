f672a39149afdf80f530e8a8cf335b9acc7f40f0
treacle74 <ptrelease@gmail.com>
2014-07-25 12:54:37 +0100

    added tests for multiple proprietor

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,8 @@ from service.server import app, db, user_datastore
 from flask_security.utils import encrypt_password
 from flask_security import current_user
 from service.models import User
+import responses
+import json
 
 import mock
 import unittest
@@ -58,6 +60,54 @@ class ViewProperyTestCase(unittest.TestCase):
     def test_404(self):
       rv = self.app.get('/pagedoesnotexist')
       assert rv.status == '404 NOT FOUND'
+
+    @responses.activate
+    def test_multiple_proprietors(self):
+      #This is the response JSON i'm mocking
+      json_dict = {
+          "payment": {
+              "price_paid": 2000000,
+              "titles": [
+                  "TEST198"
+              ]
+          },
+          "property": {
+              "address": {
+                  "house_number": "Flat 11 Queensmere Court",
+                  "postcode": "SW13 9AT",
+                  "road": "Verdun Road",
+                  "town": "London"
+              },
+              "class_of_title": "absolute",
+              "tenure": "leasehold"
+          },
+          "proprietors": [
+              {
+                  "first_name": "Bob",
+                  "last_name": "Test"
+              },
+              {
+                  "first_name": "Betty",
+                  "last_name": "Tanker"
+              }
+          ],
+          "title_number": "TEST198"
+      }
+
+      response_json = json.dumps(json_dict)
+
+      #Mock a response, as though JSON is coming back from SEARCH_API
+      responses.add(responses.GET, '%s/auth/titles/%s' % (self.search_api, TITLE_NUMBER),
+          body = response_json, status = 200, content_type='application/json')
+
+      #Now call the usual Service frontend for the same title.  Redirects
+      #to the mocked response in HTML.
+      self._login('landowner@mail.com', 'password')
+      rv = self.app.get('/property/%s' % TITLE_NUMBER, follow_redirects=True)
+      assert rv.status_code == 200
+      assert 'Bob Test' in rv.data
+      assert 'Betty Tanker' in rv.data
+
 
     def tearDown(self):
       self.logout() #to ensure no-one is logged in after a test is run
