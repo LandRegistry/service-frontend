d316c465dcba559686ed8bab5a66f1dc175e0702
Adam Shimali <adam.shimali@gmail.com>
2014-07-28 14:53:59 +0100

    Fix db user/users shenanigans

--- service/models.py
+++ service/models.py
@@ -3,7 +3,7 @@ from flask.ext.security import UserMixin, RoleMixin
 from service import db
 
 roles_users = db.Table('roles_users',
-        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('users_id', db.Integer(), db.ForeignKey('users.id')),
         db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 class Role(db.Model, RoleMixin):
@@ -12,6 +12,9 @@ class Role(db.Model, RoleMixin):
     description = db.Column(db.String(255))
 
 class User(db.Model, UserMixin):
+
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
