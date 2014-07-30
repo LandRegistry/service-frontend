f4059d0ca1faf1c4b889dff018a5d31e5db8326f
Adam Shimali <adam.shimali@gmail.com>
2014-07-30 15:31:47 +0100

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,6 +1,6 @@
 export SETTINGS='config.DevelopmentConfig'
 export AUTHENTICATED_SEARCH_API='http://localhost:8003'
-export SECRET_KEY='''T\xdf\xa2\x0f\x10&\x10\xad9\xa5\xd0\xa6f\x88\x95`\xb0"\x10F\x19L\x89S'''
+export SECRET_KEY='local-dev-not-secret'
 export TEST_USERNAME='test_user@email.com'
 export TEST_PASSWORD='password'
 
