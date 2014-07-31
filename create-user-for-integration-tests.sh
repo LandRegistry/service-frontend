4b36b2d8a5b8184c772e6c0f12c80581cf2df081
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-07-31 13:16:34 +0100

    Made compatible with new dev-env layout

--- create-user-for-integration-tests.sh
+++ create-user-for-integration-tests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-source /vagrant/script/checkout
+# TODO: clean up, we're maintaining compatibility with both dev envs
+
+if [[ -f /vagrant/script/checkout ]]; then 
+	source /vagrant/script/checkout
+else
+	source /vagrant/script/dev-env-functions
+fi
+
 source ./environment.sh
 create_virtual_env "service-frontend"
 python manage.py create_user --email=geoff@gmail.com --password=apassword
