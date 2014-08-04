07b3c8771314aeb210f8dbd9e226613274266b60
mooreandrew <mooreandrewlr@gmail.com>
2014-08-04 15:57:12 +0100

    Changed test username and passed

--- create-user-for-integration-tests.sh
+++ create-user-for-integration-tests.sh
@@ -3,6 +3,6 @@
 source /vagrant/script/dev-env-functions
 source ./environment.sh
 create_virtual_env "service-frontend"
-python manage.py create_user --email=geoff@gmail.com --password=apassword
+python manage.py create_user --email=citizen@example.org --password=dummypassword
 
 deactivate
