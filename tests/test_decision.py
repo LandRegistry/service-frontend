f0da23b06dad728b60a588c6ddd61320df50f724
Juan Matthys Uys <opyate@gmail.com>
2014-08-18 13:32:33 +0100

    post to decision box

---
+++ tests/test_decision.py
@@ -0,0 +1,26 @@
+from service.server import app
+from service.decision import Decision
+import mock
+import unittest
+
+
+
+class DecisionTestCase(unittest.TestCase):
+    """
+    Tests the decision wrapper in decision module.
+    """
+
+    def setUp(self):
+        self.client = app.test_client()
+
+
+    @mock.patch('requests.post')
+    def test_post(self, mock_post):
+        dummy_url = 'dummy'
+        dummy_data = {'marriage_country':'GB'}
+        
+        decision = Decision(dummy_url)
+        decision.post(dummy_data)
+
+        assert len(mock_post.call_args_list) == 2
+
