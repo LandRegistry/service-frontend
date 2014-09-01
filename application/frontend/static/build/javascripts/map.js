eab55233ffe8045b13ee9fa0b2167db5ded39485
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2014-09-01 12:31:35 +0000

    show extent on top of easement

--- application/frontend/static/build/javascripts/map.js
+++ application/frontend/static/build/javascripts/map.js
@@ -35,22 +35,24 @@ map.addLayer(openspaceLayer);
 //Define name of CRS in GeoJSON using PROJ4
 proj4.defs("urn:ogc:def:crs:EPSG:27700","+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs");
 
-//Add the extentGeoJson to the map
+//Create the extent layer
 var extentGeoJson = L.Proj.geoJson(extentData, {
   color: 'red',
   fillOpacity: 0.0
 })
-extentGeoJson.addTo(map);
 
-//Add the extentGeoJson to the map
+//Create the easement layer
 if (easementData) {
   var easementGeoJson = L.Proj.geoJson(easementData, {
     color: 'blue',
     fillcolor: 'blue',
     fillOpacity: 0.5
   })
-  easementGeoJson.addTo(map);
 };
+
+//Add layers to the map
+easementGeoJson.addTo(map);
+extentGeoJson.addTo(map);
 
 //Add a scale control to the map
 L.control.scale().addTo(map);
