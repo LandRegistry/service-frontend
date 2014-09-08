31e3aad6da129384e9d823f00da558e838597903
Simon Tsang <simon.tsang@gmail.com>
2014-09-08 14:38:13 +0000

    WIP: creating MVC for relationships

---
+++ application/frontend/controllers/relationship_controller.py
@@ -0,0 +1,28 @@
+class RelationshipController(object):
+
+steps = [handle_client_start, handle_client_enter_token, handle_client_confirm, handle_client_confirmed]
+
+def handle():
+    if request.session['step']
+        current_step = request.session['step']
+
+        if current_step > len(steps)
+            error
+
+        next_step = steps[current_step + 1]
+
+
+def __handle_client_start():
+    return render_template('client-start.html')
+
+@login_required
+def __handle_client_enter_token():
+    return render_template('client-enter-token.html')
+
+@login_required
+def __handle_client_confirm():
+    return render_template('client-confirm.html')
+
+@login_required
+def __handle_client_confirmed():
+    return render_template('client-confirmed.html')
