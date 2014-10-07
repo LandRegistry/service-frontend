6dca87875774f1f5a60b5a485f601860d332338a
Simon Tsang <simon.tsang@gmail.com>
2014-10-07 14:18:56 +0100

    adding script functions to block and unblock users

---
+++ block_user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /vagrant/script/dev-env-functions
+source ./environment.sh
+create_virtual_env "service-frontend"
+
+python manage.py block_user --email=$1
+deactivate
+
+echo
+echo
+echo "Blocked user"
+echo
+echo
---
+++ unblock_user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /vagrant/script/dev-env-functions
+source ./environment.sh
+create_virtual_env "service-frontend"
+
+python manage.py unblock_user --email=$1
+deactivate
+
+echo
+echo
+echo "User unblocked"
+echo
+echo
