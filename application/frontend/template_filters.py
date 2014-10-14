3fa6f659c6042409e56dd908a193e843cbb7a966
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2014-10-14 15:08:27 +0100

    Remove comments

--- application/frontend/template_filters.py
+++ application/frontend/template_filters.py
@@ -3,8 +3,6 @@ import dateutil
 
 def dateformat(value, format = '%-d %B %Y'):
     new_date =dateutil.parser.parse(value, dayfirst=True)
-    # if new_date.strftime(format)[0] == '0':
-    #     new_date = new_date
     return new_date.strftime(format)
 
 def datetimeformat(value, format = '%-d %B %Y at %H:%M:%S'):
