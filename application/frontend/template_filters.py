3e0e6d5905e85766a2b82928a44e4b80ff7cf67f
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2014-10-14 15:07:36 +0100

    Update to the dateformat filter. Added tests.

--- application/frontend/template_filters.py
+++ application/frontend/template_filters.py
@@ -1,13 +1,15 @@
 import dateutil
 
 
-def dateformat(value, format = '%d %B %Y'):
-    new_date =dateutil.parser.parse(value)
+def dateformat(value, format = '%-d %B %Y'):
+    new_date =dateutil.parser.parse(value, dayfirst=True)
+    # if new_date.strftime(format)[0] == '0':
+    #     new_date = new_date
     return new_date.strftime(format)
 
-def datetimeformat(value):
+def datetimeformat(value, format = '%-d %B %Y at %H:%M:%S'):
     norm = dateutil.parser.parse(value)
-    return norm.strftime('%d %B %Y at %H:%M:%S')
+    return norm.strftime(format)
 
 def currency(value):
     """Format a comma separated  currency to 2 decimal places."""
