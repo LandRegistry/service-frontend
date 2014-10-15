842a6711644a1bca17b0b2825db739632f56a697
Colm Britton <colmjude@gmail.com>
2014-10-15 10:13:35 +0100

    correct rebase issue

--- application/frontend/forms.py
+++ application/frontend/forms.py
@@ -66,25 +66,3 @@ class ConfirmForm(ChangeForm):
     marriage_country = HiddenField('Country of marriage ceremony')
     marriage_certificate_number = HiddenField('Marriage certificate number')
 
-
-class ConveyancerAddClientForm(Form):
-    full_name = StringField('Full name', validators=[DataRequired()])
-    date_of_birth = DateField('Date of birth', format='%d-%m-%Y',
-                              validators=[DataRequired(), ValidateDateNotInFuture()],
-                              description="For example, 20-08-2011")
-    address = TextAreaField('Address', validators=[DataRequired()])
-    telephone = StringField('Mobile phone number', validators=[DataRequired()], description="this may be used to receive security codes")
-    email = StringField('Email address', validators=[DataRequired(), Email()])
-
-    # this is temporary for matching
-    gender = RadioField('Gender', choices=[('F', 'Female'), ('M', 'Male')], validators=[DataRequired()])
-
-
-class SelectTaskForm(Form):
-    buying_or_selling_property = RadioField(
-        'Is your client buying or selling this property?',
-        choices=[
-            ('buying', 'Buying'),
-            ('selling', 'Selling')
-        ],
-        validators=[DataRequired()])
