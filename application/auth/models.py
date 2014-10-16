691a53fe326ded36de86f9a8a9178473c8ec423b
Adam Shimali <adam@macmoon.local>
2014-10-16 14:37:38 +0100

    Fix import

--- application/auth/models.py
+++ application/auth/models.py
@@ -14,7 +14,7 @@ from application import (
     app
 )
 
-from password import PasswordUtils
+from lrutils import PasswordUtils
 password_utils = PasswordUtils(app.config)
 
 class User(db.Model, UserMixin):
